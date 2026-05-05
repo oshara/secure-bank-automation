43a08b7fb82262b9bd68d94ff30ca8dde5286f86
oshara <amanthi1995@gmail.com>
2026-05-05 22:32:09 +0530

    add the dasbhoard page

---
+++ pages/dashboard/dashboard.js
@@ -0,0 +1,30 @@
+import { expect } from "@playwright/test";
+
+export class DashboardPage {
+    constructor(page){
+        this.page = page;
+        this.dashboardContainer = page.locator('[id="dashboard-page-container"]');
+        this.skelatonCardLocator = page.locator('[data-testid="skeleton-card"]').nth(0);
+
+        // Total Balance Card
+        this.totalBalanceCard = page.locator("[data-testid='total-balance-card']");
+
+        // Active Account Card
+        this.activeAccountCard = page.locator("[data-testid='total-balance-card']");
+
+        //Total Transacations Card
+        this.totalTransactionsCard = page.locator("[data-testid='transactions-count-card']");
+
+        
+    }
+
+    async skelatonLoadingStateValidation(){
+           await expect(this.dashboardContainer).toHaveAttribute('data-loading','true');
+           await expect(this.skelatonCardLocator).toBeVisible();
+           await expect(this.dashboardContainer).toHaveAttribute('data-loading','false',{timeout:2000});
+
+           await expect(this.totalBalanceCard).toContainText(/\$\d+/);
+           await expect(this.activeAccountCard).toContainText(/\d+/);
+           await expect(this.totalTransactionsCard).toHaveText(/\d+/);
+        }
+}
